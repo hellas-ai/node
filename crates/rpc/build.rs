59f31319d0f0640639abb6dd5dcf10ddc0a7c1be
georgewhewell <georgerw@gmail.com>
2026-01-08 13:11:12 +0100

    tidy: use prost::Name

--- crates/rpc/build.rs
+++ crates/rpc/build.rs
@@ -6,13 +6,16 @@ fn main() {
 #[cfg(feature = "compile")]
 fn compile() {
     println!("cargo:rerun-if-changed=proto/*.proto");
+    let mut prost_config = tonic_prost_build::Config::new();
+    prost_config.enable_type_names();
+
     tonic_prost_build::configure()
         .out_dir("src/pb")
         .include_file("mod.rs")
         .emit_package(true)
         .build_client(cfg!(feature = "client"))
         .build_server(cfg!(feature = "server"))
-        .build_transport(cfg!(feature = "transport"))
-        .compile_protos(&["proto/hellas.proto"], &["proto"])
+        .build_transport(false) // we use our own transport
+        .compile_with_config(prost_config, &["proto/hellas.proto"], &["proto"])
         .expect("Failed to compile protos");
 }
